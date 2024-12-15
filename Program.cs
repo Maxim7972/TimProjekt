23f77f224784db0de69ec05c4117ab9fbcc32a5b
Maxim7972 <120516216+Maxim7972@users.noreply.github.com>
2024-12-15 13:49:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
 
+
+public class Exam : IDateAndCopy
+{
+    public string Name { get; set; }      
+    public int Note { get; set; }        
+    public DateTime Date { get; set; }   
+
+    
+    public Exam(string name, int note, DateTime examDate)
+    {
+        this.Name = name;
+        this.Note = note;
+        this.Date = examDate;
+    }
+
+   
+    public Exam() { }
+
+    // Реализация метода Copy()
+    public object Copy()
+    {
+        return new Exam(this.Name, this.Note, this.Date);
+    }
+
+    public override string ToString()
+    {
+        return $"Предмет: {Name}, Оценка: {Note}, Дата экзамена: {Date.ToShortDateString()}";
+    }
+}
+
+
+
+
